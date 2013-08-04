b3ab1575926be34a5fa575117d2c323d48bc7964
guowei <limguowei.10@ichat.sp.edu.sg>
2013-08-04 18:39:52 +0800

    Remove friefunk-firewall

--- luci.sh
+++ luci.sh
@@ -18,9 +18,6 @@ sed -i 's|height: 1em;|height: 2em;|g' $HOME/openwrt/TL-WR1043ND/attitude_adjust
 #rm $HOME/openwrt/TL-WR1043ND/attitude_adjustment/target/linux/ar71xx/base-files/etc/defconfig/wzr-hp-g301nh -rf
 #rm $HOME/openwrt/TL-WR1043ND/attitude_adjustment/target/linux/ar71xx/base-files/etc/uci-defaults/wrt160nl
 
-sed -i 's|/etc/init.d/miniupnpd disable|/etc/init.d/miniupnpd start|g' $HOME/openwrt/TL-WR1043ND/attitude_adjustment/build_dir/target-mips_r2_uClibc-0.9.33.2/luci-0.11+svn9874/ipkg-ar71xx/luci-app-upnp/etc/uci-defaults/luci-upnp
-sed -i 's|/etc/init.d/miniupnpd disable|/etc/init.d/miniupnpd start|g' $HOME/openwrt/TL-WR1043ND/attitude_adjustment/build_dir/target-mips_r2_uClibc-0.9.33.2/luci-0.11+svn9874/applications/luci-upnp/root/etc/uci-defaults/luci-upnp
-sed -i 's|/etc/init.d/miniupnpd disable|/etc/init.d/miniupnpd start|g' $HOME/openwrt/TL-WR1043ND/attitude_adjustment/build_dir/target-mips_r2_uClibc-0.9.33.2/luci-0.11+svn9874/applications/luci-upnp/dist/etc/uci-defaults/luci-upnp
 #sed -i 's"service_start /usr/sbin/nmbd -D"egrep -q 'disable.+netbios.*=.*(true|yes|1)' /var/etc/smb.conf || service_start /usr/sbin/nmbd -D"g'
 cp  $HOME/openwrt/TL-WR1043ND/attitude_adjustment/Modified_LuCI_Files/*.lua /$HOME/openwrt/TL-WR1043ND/attitude_adjustment/build_dir/target-mips_r2_uClibc-0.9.33.2/luci-0.11+svn9874/modules/admin-full/luasrc/model/cbi/admin_network/
 touch $HOME/openwrt/TL-WR1043ND/attitude_adjustment/build_dir/target-mips_r2_uClibc-0.9.33.2/luci-0.11+svn9874/modules/admin-core/root/etc/init.d/luci_fixtime
