d8a27639d00e4d5c03cad4e79a3d882886fee90f
guowei <limguowei.10@ichat.sp.edu.sg>
2013-08-25 11:16:39 +0800

    add md5 checksums

--- del.sh
+++ del.sh
@@ -1,4 +1,4 @@
- 
+#! /bin/bash 
 rm $HOME/openwrt/TL-WR1043ND/attitude_adjustment/target/linux/ar71xx/base-files/lib/upgrade/dir825.sh
 rm $HOME/openwrt/TL-WR1043ND/attitude_adjustment/target/linux/ar71xx/base-files/lib/upgrade/allnet.sh
 rm $HOME/openwrt/TL-WR1043ND/attitude_adjustment/target/linux/ar71xx/base-files/etc/defconfig/wndr3700/network
