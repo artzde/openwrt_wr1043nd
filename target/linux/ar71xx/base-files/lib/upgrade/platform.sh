ee279949ca33829b582630fb85a012f0d139a25f
guowei <limguowei.10@ichat.sp.edu.sg>
2013-08-27 16:36:36 +0800

    Modify target config

--- target/linux/ar71xx/base-files/lib/upgrade/platform.sh
+++ target/linux/ar71xx/base-files/lib/upgrade/platform.sh
@@ -77,95 +77,7 @@ platform_check_image() {
 	[ "$ARGC" -gt 1 ] && return 1
 
 	case "$board" in
-	all0315n | \
-	all0258n | \
-	cap4200ag)
-		platform_check_image_allnet "$1" && return 0
-		return 1
-		;;
-	alfa-ap96 | \
-	alfa-nx | \
-	ap113 | \
-	ap121 | \
-	ap121-mini | \
-	ap136 | \
-	ap96 | \
-	db120 | \
-	hornet-ub | \
-	zcn-1523h-2 | \
-	zcn-1523h-5)
-		[ "$magic_long" != "68737173" -a "$magic_long" != "19852003" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	ap81 | \
-	ap83 | \
-	dir-600-a1 | \
-	dir-615-c1 | \
-	dir-615-e4 | \
-	ew-dorin | \
-	ew-dorin-router | \
-	mzk-w04nu | \
-	mzk-w300nh | \
-	tew-632brp | \
-	tew-712br | \
-	wrt400n | \
-	airrouter | \
-	bullet-m | \
-	nanostation-m | \
-	rocket-m | \
-	rw2458n | \
-	wzr-hp-g300nh2 | \
-	wzr-hp-g300nh | \
-	wzr-hp-g450h | \
-	wzr-hp-ag300h | \
-	whr-g301n | \
-	whr-hp-g300n | \
-	whr-hp-gn | \
-	wlae-ag300n | \
-	nbg460n_550n_550nh | \
-	unifi | \
-	unifi-outdoor )
-		[ "$magic" != "2705" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-
-	dir-825-b1 | \
-	tew-673gru)
-		dir825b_check_image "$1" && return 0
-		;;
-
-	mr600 | \
-	om2p | \
-	om2p-hs | \
-	om2p-lc)
-		platform_check_image_openmesh "$magic_long" "$1" && return 0
-		return 1
-		;;
-	tl-mr11u | \
-	tl-mr3020 | \
-	tl-mr3040 | \
-	tl-mr3220 | \
-	tl-mr3420 | \
-	tl-wa7510n | \
-	tl-wa901nd | \
-	tl-wa901nd-v2 | \
-	tl-wdr4300 | \
-	tl-wr703n | \
-	tl-wr741nd | \
-	tl-wr741nd-v4 | \
-	tl-wr841n-v1 | \
-	tl-wr841n-v7 | \
-	tl-wr841n-v8 | \
-	tl-wr941nd | \
-	tl-wr1041n-v2 | \
-	tl-wr1043nd | \
-	tl-wr2543n)
+	tl-wr1043nd)
 		[ "$magic" != "0100" ] && {
 			echo "Invalid image type."
 			return 1
@@ -192,49 +104,6 @@ platform_check_image() {
 
 		return 0
 		;;
-	wndr3700)
-		local hw_magic
-
-		hw_magic="$(ar71xx_get_mtd_part_magic firmware)"
-		[ "$magic_long" != "$hw_magic" ] && {
-			echo "Invalid image, hardware ID mismatch, hw:$hw_magic image:$magic_long."
-			return 1
-		}
-		return 0
-		;;
-	wrt160nl)
-		[ "$magic" != "4e4c" ] && {
-			echo "Invalid image type."
-			return 1
-		}
-		return 0
-		;;
-	routerstation | \
-	routerstation-pro | \
-	ls-sr71 | \
-	pb42 | \
-	pb44 | \
-	all0305 | \
-	eap7660d | \
-	ja76pf | \
-	ja76pf2 | \
-	jwap003)
-		[ "$magic" != "4349" ] && {
-			echo "Invalid image. Use *-sysupgrade.bin files on this board"
-			return 1
-		}
-
-		local md5_img=$(dd if="$1" bs=2 skip=9 count=16 2>/dev/null)
-		local md5_chk=$(dd if="$1" bs=$CI_BLKSZ skip=1 2>/dev/null | md5sum -); md5_chk="${md5_chk%% *}"
-
-		if [ -n "$md5_img" -a -n "$md5_chk" ] && [ "$md5_img" = "$md5_chk" ]; then
-			return 0
-		else
-			echo "Invalid image. Contents do not match checksum (image:$md5_img calculated:$md5_chk)"
-			return 1
-		fi
-		return 0
-		;;
 	esac
 
 	echo "Sysupgrade is not yet supported on $board."
@@ -245,37 +114,6 @@ platform_do_upgrade() {
 	local board=$(ar71xx_board_name)
 
 	case "$board" in
-	routerstation | \
-	routerstation-pro | \
-	ls-sr71 | \
-	all0305 | \
-	eap7660d | \
-	pb42 | \
-	pb44 | \
-	ja76pf | \
-	ja76pf2 | \
-	jwap003)
-		platform_do_upgrade_combined "$ARGV"
-		;;
-	all0258n )
-		platform_do_upgrade_allnet "0x9f050000" "$ARGV"
-		;;
-	all0315n )
-		platform_do_upgrade_allnet "0x9f080000" "$ARGV"
-		;;
-	cap4200ag)
-		platform_do_upgrade_allnet "0xbf0a0000" "$ARGV"
-		;;
-	dir-825-b1 |\
-	tew-673gru)
-		platform_do_upgrade_dir825b "$ARGV"
-		;;
-	mr600 | \
-	om2p | \
-	om2p-hs | \
-	om2p-lc)
-		platform_do_upgrade_openmesh "$ARGV"
-		;;
 	*)
 		default_do_upgrade "$ARGV"
 		;;
